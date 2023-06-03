e3d2a2deb9e73d6eec8d80a342740bf16619b578
EvgenyDavydenko <frontend82@mail.ru>
2023-06-03 16:19:50 +0300

    persons cards

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Examples from Bogdan Stashchuk React-18 course
 
+### master
+
 1.  Initial setup
+
+### 14-map-array-of-objects
+
+2.  persons cards
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Persons from "./components/Persons";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>Карточки пользователей</h1>
+      <Persons />
     </div>
   );
 }
